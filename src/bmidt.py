6edd75914f1e470c31f595b011b06ceac6485823
Benjamin Midtvedt <benmid@student.chalmers.se>
2020-11-02 15:06:33 +0100

    set root

--- src/bmidt.py
+++ src/bmidt.py
@@ -110,7 +110,9 @@ for prod in itertools.product(*TEST_VARIABLES.values()):
     _generators.append(
         (
             arguments,
-            lambda: apido.get_generator(**arguments),
+            lambda: apido.get_generator(
+                root_path="E:/hackathon/", **arguments
+            ),
         )
     )
 
