ffe54829663712f86e2431aa6958ce0cbff0904f
JesusPinedaC <jesus.pineda@physics.gu.se>
2020-11-02 18:31:27 +0100

    set up model

--- src/jpicas.py
+++ src/jpicas.py
@@ -6,15 +6,27 @@ from tensorflow.keras.initializers import RandomNormal
 
 
 TEST_VARIABLES = {
-    "generator_depth": [4, 5, 6],
+    "generator_depth": [4, 5],
     "generator_base_breadth": [16],
+    "assemble_loss_weights": [0.8, 4, 10],
+    #"alpha": [0.1, 0.2, 0.5],
+    "alpha" : [0.2]
+    "min_data_size": [256],
+    "max_data_size": [512],
+    "batch_size": [16],
 }
 
 
-def model_initializer(generator_depth, generator_base_breadth, **kwargs):
+def model_initializer(
+    generator_depth,
+    generator_base_breadth,
+    alpha,
+    assemble_loss_weights,
+    **kwargs
+):
 
     kernel_initializer = RandomNormal(mean=0.0, stddev=0.02)
-    activation = lambda x: layers.LeakyReLU(0.2)(x)
+    activation = lambda x: layers.LeakyReLU(alpha)(x)
 
     convolution_block = dt.layers.ConvolutionalBlock(
         activation=activation,
@@ -38,7 +50,7 @@ def model_initializer(generator_depth, generator_base_breadth, **kwargs):
     )
 
     generator = dt.models.unet(
-        input_shape=(None, None, 1),  # shape of the input
+        input_shape=(None, None, 7),  # shape of the input
         conv_layers_dimensions=list(
             generator_base_breadth * 2 ** n for n in range(generator_depth)
         ),  # number of features in each convolutional layer
@@ -50,7 +62,7 @@ def model_initializer(generator_depth, generator_base_breadth, **kwargs):
             generator_base_breadth,
         ),  # number of features in convolutional layer after the U-net
         steps_per_pooling=2,  # 2                                 # number of convolutional layers per pooling layer
-        number_of_outputs=1,  # number of output features
+        number_of_outputs=3,  # number of output features
         output_activation="tanh",  # activation function on final layer
         compile=False,
         output_kernel_size=1,
@@ -81,7 +93,7 @@ def model_initializer(generator_depth, generator_base_breadth, **kwargs):
     )
 
     discriminator = dt.models.convolutional(
-        input_shape=(256, 256, 1),  # shape of the input
+        input_shape=[(256, 256, 3), (256, 256, 7)],  # shape of the input
         aux_input_shape=(256, 256, 1),
         conv_layers_dimensions=(
             16,
@@ -110,6 +122,7 @@ def model_initializer(generator_depth, generator_base_breadth, **kwargs):
         discriminator_metrics="accuracy",
         assemble_loss=["mse", apido.combined_metric()],
         assemble_optimizer=Adam(lr=0.0002, beta_1=0.5),
+        assemble_weights=[1, 0.8],
     )
 
     return model
@@ -127,7 +140,8 @@ for prod in itertools.product(*TEST_VARIABLES.values()):
         (
             arguments,
             lambda: apido.get_generator(
-                min_data_size=16, max_data_size=1000, batch_size=8
+                root_path=r"C:/Users/Jesus/OneDrive/Documents/ACChallenge/files/60x images/",
+                **arguments
             ),
         )
     )
